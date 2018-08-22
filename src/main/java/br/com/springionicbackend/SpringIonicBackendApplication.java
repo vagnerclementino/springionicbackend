1c9330c16175ec9142690af58a0dfad4cc1babd6
Vagner Clementino <vagner.clementino@gmail.com>
2018-08-22 15:20:40 -0300

    Criação do projeto

---
+++ src/main/java/br/com/springionicbackend/SpringIonicBackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.springionicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicBackendApplication.class, args);
+	}
+}
